651bdc1047842c72cba936872bc81fbc0556cb51
Mariana Vida <mariana-vida@hotmail.com>
2022-10-07 20:49:17 -0300

    aula07/10

---
+++ lista01.2/exercicio14.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+  // Faça um programa que receba o salário inicial de um
+  // funcionário, calcule e mostre o novo salario, acrescido de
+  // bonificação e de auxilio escola
+
+  // salario ate 500 - bonificação 5%
+  // salario entre 500 e 1200 - bonificação 12%
+  // salario acima de 1200 - sem bonificação
+
+  // salario ate 600 - auxilio 150
+  // salario acima 600 - auxilio 100
+}
